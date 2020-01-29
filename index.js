9ddd7e27638351f0a32948fd3559be1e0bbf3e68
Melissa Astbury <melissajastbury@gmail.com>
2020-01-29 19:23:00 +0000

    Introduction to While Loops

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
-// SECTION ONE
-// create secretNumber
-var secretNumber = 4;
+// // SECTION ONE - CONDITIONALS
+// // create secretNumber
+// var secretNumber = 4;
 
-//ask user for guess
-var guess = prompt("Guess a number");
+// //ask user for guess
+// var guess = prompt("Guess a number");
 
-//check if guess is right
-if (Number(guess) === secretNumber) {
-  alert("YOU GOT IT RIGHT!");
+// //check if guess is right
+// if (Number(guess) === secretNumber) {
+//   alert("YOU GOT IT RIGHT!");
+// }
+
+// //check if guess is higher
+// else if (Number(guess) > secretNumber) {
+//   alert("Too high. Guess again...");
+// }
+
+// //otherwise, check if lower
+// else {
+//   alert("Too low. Guess again...");
+// }
+
+//SECTION TWO - LOOPS
+// Print all numbers between -10 & 19
+console.log("Printing all numbers between -10 & 19");
+var counter = -10;
+
+while (counter < 20) {
+  console.log(counter);
+  counter++;
 }
 
-//check if guess is higher
-else if (Number(guess) > secretNumber) {
-  alert("Too high. Guess again...");
+//Print all even numbers between 10 & 40
+console.log("Printing all even numbers between 10 & 40");
+var counter = 10;
+
+while (counter <= 40) {
+  console.log(counter);
+  counter += 2;
 }
 
-//otherwise, check if lower
-else {
-  alert("Too low. Guess again...");
+//Print all odd numbers between 300 & 333
+console.log("Printing all odd numbers between 300 & 333");
+var counter = 300;
+
+while (counter <= 333) {
+  if (counter % 2 !== 0) {
+    console.log(counter);
+  }
+  counter += 1;
+}
+
+//Print all numbers divisable by 5 & 3 between 5 & 50
+console.log("Printing all numbers divisable by 5 & 3 between 5 & 50");
+var counter = 5;
+
+while (counter <= 5) {
+  if (counter % 5 === 0) {
+    console.log(counter);
+  }
+  counter += 1;
 }
